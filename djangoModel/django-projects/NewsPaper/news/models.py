26a2ed16a00eb55f46939d993ffb21916b5c1baf
valah75 <94748169+valah75@users.noreply.github.com>
2022-08-26 22:57:05 +0300

    News Portal project

---
+++ djangoModel/django-projects/NewsPaper/news/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+news = 'NE'
+article = 'AR'
+
+TYPES = [
+    (news, 'Новость'),
+    (article, 'Статья')
+    ]
+# Create your models here.
+class Author(models.Model):
+    rating = models.IntegerField(default = 0)
+    user = models.OneToOneField(User, default=0, on_delete = models.CASCADE)
+
+    def update_rating(userid):
+    #    Author.objects.filter(id=userid, Post.kind="")
+    #    Author.objects.filter(age__lt=25)
+        pass
+
+
+class Category(models.Model):  # наследуемся от класса Model
+    name = models.CharField(max_length = 128, unique = True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    kind = models.CharField(max_length=255, choices=TYPES)
+    creation_date = models.DateTimeField(auto_now_add = True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    header = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default = 0)
+
+    def like(self):
+        self.rating = self.rating + 1
+        self.save()
+
+    def dislike(self):
+        self.rating = self.rating - 1
+        self.save()
+
+    def preview(self):
+        return self.text[::124] + "..."
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add = True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating = self.rating + 1
+        self.save()
+
+    def dislike(self):
+        self.rating = self.rating - 1
+        self.save()
+
